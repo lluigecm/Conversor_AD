d7d6897e6663c70a3e872b947676ba751f806d85
Luige <luige030301@gmail.com>
2025-02-14 17:13:55 -0300

    fix update_square, fix irq handler, main code finished

--- Tarefa_U4_C8.c
+++ Tarefa_U4_C8.c
@@ -38,6 +38,7 @@ bool joystick_leds = true;  // Variável para controle dos LEDs do Joystick
 uint last_time = 0;     // Variável para debounce
 
 uint16_t joystick_center = MAX_ADC/2; // Centro do Joystick
+
 uint16_t x_loc = 60;// ----|
 uint16_t y_loc = 28;//     |Localização inicial do quadrado
 
@@ -53,13 +54,13 @@ void init_gpio_pwm(uint gpio){
 uint16_t adc_to_pwm(uint16_t adc_value){ // Converte o valor do ADC para o PWM considerando a dead zone do joystick
     
     // se estiver dentro da deadzone, retorna zero -> Não movimenta o quadrado
-    if(adc_value > joystick_center - DEADZONE && adc_value < joystick_center + DEADZONE)
+    if(adc_value > (joystick_center - DEADZONE) && adc_value < (joystick_center + DEADZONE))
         return 0;
     // Retorna o PWM para regiao menor que o centro -> Movimenta do quadrado para o lado esquerdo
-    if(adc_value < joystick_center - DEADZONE)
+    if(adc_value < (joystick_center - DEADZONE))
         return (MAX_ADC - (adc_value * 2));
     // Retorna o PWM para regiao maior que o centro -> Movimenta do quadrado para o lado direito
-    if (adc_value > joystick_center + DEADZONE)
+    if (adc_value > (joystick_center + DEADZONE))
         return (adc_value - (joystick_center + DEADZONE)) * 2;
 }
 
@@ -132,16 +133,39 @@ void buttons_handler(uint gpio, uint32_t events){
             update_border(); // Atualiza a borda do display
         }
         if(gpio == BUTTON_A){
-            if(joystick_leds){
-                pwm_set_gpio_level(RED_LED, adc_to_pwm(joy_x));
-                pwm_set_gpio_level(BLUE_LED, adc_to_pwm(joy_y));
-            }else{
-                pwm_set_gpio_level(RED_LED, 0);
-                pwm_set_gpio_level(BLUE_LED, 0); 
-            }
             joystick_leds = !joystick_leds;
         }
     }
+}
+
+void update_square(uint16_t x, uint16_t y){
+    uint16_t last_x = x_loc;
+    uint16_t last_y = y_loc;
+
+    if(x > (joystick_center - DEADZONE) && x < (joystick_center + DEADZONE))
+        x_loc = 60;
+    else
+        x_loc = (x * 128) / MAX_ADC;
+
+    if(y > (joystick_center - DEADZONE) && y < (joystick_center + DEADZONE))
+        y_loc = 28;
+    else
+        y_loc = ((MAX_ADC - y) * 64) / MAX_ADC; // Inverte o eixo y
+
+    // Mantém o quadrado dentro do display
+    if(x_loc < 2) 
+        x_loc = 1;
+    if(x_loc > (WIDTH - 1) - SQUARE_SIZE- 2)
+        x_loc = (WIDTH - 1) - SQUARE_SIZE - 2;  
+    if(y_loc < 2)
+        y_loc = 1;
+    if(y_loc > (HEIGHT - 1) - SQUARE_SIZE - 2)
+        y_loc = (HEIGHT - 1) - SQUARE_SIZE - 2;
+
+    // Update square position
+    ssd1306_rect(&ssd, last_y, last_x, SQUARE_SIZE, SQUARE_SIZE, false, true); // Limpa o quadrado
+    ssd1306_rect(&ssd, y_loc, x_loc, SQUARE_SIZE, SQUARE_SIZE, true, true); // Desenha o quadrado
+    ssd1306_send_data(&ssd); // Envia os dados para o display
 }
 
 
@@ -151,6 +175,10 @@ int main()
     setup();
     
     display_init_config();
+    sleep_ms(2000); // Pausa de 2s
+    ssd1306_fill(&ssd, !cor); // Limpa o display
+    ssd1306_rect(&ssd, 0, 0, WIDTH-1, HEIGHT-1, cor, false); // Desenha a borda do display
+    ssd1306_send_data(&ssd); // Envia os dados para o display
 
     gpio_set_irq_enabled_with_callback(JOYSTICK_BUTTON, GPIO_IRQ_EDGE_FALL, true, &buttons_handler); // Habilita a interrupção no botão do Joystick
     gpio_set_irq_enabled_with_callback(BUTTON_A, GPIO_IRQ_EDGE_FALL, true, &buttons_handler); // Habilita a interrupção no botão A
@@ -161,6 +189,17 @@ int main()
         adc_select_input(1);
         joy_x = adc_read();
 
+        update_square(joy_x, joy_y);
+
+        if(joystick_leds){//------------------------------|
+            joy_x = adc_to_pwm(joy_x);//                  | Esse trecho do codigo responsavel por controlar a intesidade 
+            joy_y = adc_to_pwm(joy_y);//                  | dos leds de acordo com a conversão do valor do joystick para PWM,
+            pwm_set_gpio_level(RED_LED, joy_x);//         | devvia ficar na funcão callback de IRQ, mas por algum motivo
+            pwm_set_gpio_level(BLUE_LED, joy_y);//        | não estava sendo executada quando deveria. 
+        }else{//                                          | Entao foi colocado aqui para garantir que a intensidade dos leds
+            pwm_set_gpio_level(RED_LED, 0);//             | seja modificada de acordo com o movimento do joystick.
+            pwm_set_gpio_level(BLUE_LED, 0); //           |  
+        }//-----------------------------------------------|
 
         sleep_ms(50); // pausa entre iterações
     }
